1b4035a22000f2e27f769f89c7456f0b6e1e6237
luc-caspar <luc.caspar@cross-compass.com>
2026-02-24 11:44:52 +0900

    Implement a simple ball falling through the environment.

--- test_pymunk.py
+++ test_pymunk.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
 from time import sleep
 import pymunk as pm
 import pyglet as pg
-from threading import Thread, Event
-from queue import Queue, Empty
+from multiprocessing import Process, Event, Queue
+from queue import Empty
+from settings import FPS
 
-
-FPS = 60
 
 # /!\ Pyglet coordinate system originates in the lower-left corner
-class Display(Thread):
-    """Defines a threaded window to display the environment's state."""
+class Display(Process):
+    """Defines a window to display the environment's state."""
 
     def __init__(self, q, evt):
         """
@@ -26,59 +26,100 @@ class Display(Thread):
         # Initialize the parent class
         super().__init__()
 
-        self._win = pg.window.Window()
+        self._win = None  # This is necessary otherwise the openGL environment is not correctly initialized
         self._q = q
         self._evt = evt
 
-    def on_draw(self):
+    def on_close(self):
         # Let the main thread now that the window closed
-        if self._win.has_exit:
-            self._evt.set()
-        else:
+        self._evt.set()
+        # Propagate the standard closing process
+        return self._win.on_close()
+
+    def on_draw(self):
+        if not self._win.has_exit:
+            batch = pg.graphics.Batch()
             # Get the data to display next
             try:
-                data = self._q.get(timeout=3)
+                data = self._q.get(timeout=1)
             except Empty:
                 # Nothing new, closing the window
                 self._win.close()
+                return 
 
             # Clear the window
             self._win.clear()
 
             # TODO: Update display according to content of `data`
+            for obj in data:
+                if obj[0] == 'Circle':
+                    g = pg.shapes.Circle(obj[2], obj[3], radius=obj[4], color=(255, 0, 0), batch=batch)
+            batch.draw()
+
 
     def run(self):
+        # Initialize the window
+        self._win = pg.window.Window()
         # Let the window know how to draw things
-        self._win.push_handlers(on_draw=self.on_draw)
+        self._win.push_handlers(on_draw=self.on_draw, on_close=self.on_close)
 
-        try:
+#        try:
             # Run the loop forever
-            pg.app.run(interval=1/FPS)
-        except RuntimeError:
-            if not self._evt.is_set():
-                self._evt.set()
+        pg.app.run(interval=1/FPS)
+#        except RuntimeError:
+#            if not self._evt.is_set():
+#                self._evt.set()
 
 
 if __name__ == "__main__":
+    # Declare a command line interface
+    parser = ArgumentParser()
+    parser.add_argument('--headless',
+                        dest='headless',
+                        action='store_true',
+                        help='A flag indicating whether to run the simulation in headless mode or not.')
+
+    # Gather any command line argument
+    args = parser.parse_args()
+
     # Create the environment
     env = pm.Space()
     env.gravity = (0, -9.81)
 
-    # Instantiate a window to display the state
-    q = Queue()
-    end_evt = Event()
-    disp = Display(q, end_evt)
-    disp.start()
+    # But a at altitude in the environment
+    bdy = pm.Body()
+    ball = pm.Circle(body=bdy, radius=10)
+    ball.elasticity = 1  # perfect bounce
+    ball.friction = 0  # frictionless
+    ball.mass = 3  # mass defined on shape. Body mass and moment will be computed when object is added to environment
 
-    # Let the world evolve
-    cnt = 0
-    while not end_evt.is_set():  # Do not stop the simulation if the display closes
-        cnt += 1
-        env.step(1 / FPS)
-        # TODO: Stop queuing data if window is closed (i.e.: `end_evt.is_set()`)
-        sleep(1 / FPS)  # Do not wait for the display to move on with the simulation
-        if cnt == 180:
-            end_evt.set() # Do not close the display when the simulation is done
+    # Add the ball in the environment
+    bdy.position = (100, 100)
+    env.add(bdy, ball)
 
-    # Wait for the display to close
-    disp.join()
+    # Instantiate display thread to monitor environment state if necessary
+    if not args.headless:
+        q = Queue()
+        end_evt = Event()
+        disp = Display(q, end_evt)
+        disp.start()
+
+    try:
+        # Let the world evolve
+        cnt = 0
+        while bdy.position.y - ball.radius > 0:  # /!\ Do not stop the simulation if the display closes
+            env.step(1 / FPS)
+
+            # Do not queue data if the display thread does not exist
+            if not (args.headless or end_evt.is_set()):
+                # Send only the shapes to the display thread
+                shapes = []
+                for shape in env.shapes:
+                    if type(shape) == pm.shapes.Circle:
+                        shapes.append(('Circle', shape.body.id, shape.body.position.x, shape.body.position.y, shape.radius))
+                q.put(list(shapes))
+
+    finally:
+        # Wait for the display to close if necessary
+        if not args.headless:
+            disp.join()
